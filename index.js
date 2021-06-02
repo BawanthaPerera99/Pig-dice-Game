f7526eacebe2bc10fcefe392aeffe2e404e9961f
BawanthaPerera99 <61136367+BawanthaPerera99@users.noreply.github.com>
2021-06-02 10:47:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomNumber1;
+randomNumber1 = (Math.floor(Math.random() * 6)) + 1;
+
+var randomImage1 = "images/dice" + randomNumber1 + ".png";
+document.querySelector(".img1").setAttribute("src", randomImage1);
+
+var randomNumber2;
+randomNumber2 = (Math.floor(Math.random() * 6)) + 1;
+
+var randomImage2 = "images/dice" + randomNumber2 + ".png";
+document.querySelector(".img2").setAttribute("src", randomImage2);
+
+alert(randomNumber1 + " vs " + randomNumber2);
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").textContent = "🚩Player 1 wins";
+}else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").textContent = "Player 2 wins🚩";
+}else {
+    document.querySelector("h1").textContent = "No one wins 🤷‍♀. Scores are tied";
+}
